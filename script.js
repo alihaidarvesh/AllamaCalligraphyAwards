7d64e98fa095ed6701e0ee36699db4b7b532fc74
alihaidarvesh <mpralihaider@gmail.com>
2026-01-09 01:15:14 +0530

    Update script.js

--- script.js
+++ script.js
@@ -25,5 +25,23 @@ const data = [
     winner:true,
     image:"C.png",
     scores:[9,9,8,8,6]
+  },
+  {
+    entry:"E",
+    total:37,
+    award:"🥉 Third (Joint)",
+    badge:"bronze",
+    winner:true,
+    image:"E.png",
+    scores:[8,8,7,7,7]
+  },
+  {
+    entry:"H",
+    total:37,
+    award:"🥉 Third (Joint)",
+    badge:"bronze",
+    winner:true,
+    image:"H.png",
+    scores:[8,8,7,7,7]
   }
 ];
